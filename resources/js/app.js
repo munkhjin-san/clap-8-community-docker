9999b1b06813610502f61c01b262ab7f88a410ed
tumur <tumur.ulambayar@glowd.co.jp>
2023-11-21 17:26:24 +0900

    message search text overflow fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,19 +7,6 @@ import i18n from './plugins/i18n';
 import MasonryWall from '@yeger/vue-masonry-wall'
 import VueLazyload from 'vue3-lazyload';
 const app = createApp({});
-import * as PusherPushNotifications from "@pusher/push-notifications-web";
-
-// if ("serviceWorker" in navigator) {
-//     navigator.serviceWorker
-//       .register("/service-worker.js")
-//       .then(function (reg) {
-//         console.log("Yes, it did.");
-//       })
-//       .catch(function (err) {
-//         console.log("No it didn't. This happened:", err);
-//       });
-//   }
-
 
 
 
@@ -46,56 +33,8 @@ if(theme){
         document.documentElement.style.setProperty(pallete.className, dark ? pallete.dark : pallete.light);
     });
 } 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
-// // Pusher.logToConsole = true;
-// if(store.state.user && store.state.user.id){
-//     console.log('pusher init')
-//     let pusher = new Pusher(process.env.MIX_PUSHER_APP_KEY, {
-//         cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//         forceTLS: true
-//     });
-//     var channel = pusher.subscribe('private-chat');
-//     channel.bind('my-event', (e) => {
-//         if(e.message.title && e.message.sender !== store.state.user.id && e.message.board_members.includes(store.state.user.id)){
-//             if (Notification.permission === 'granted' && !store.state.focused) {
-//                 if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
-//                     console.log(e)
-//                     navigator.serviceWorker.controller.postMessage({
-//                         type: 'pushNotification',
-//                         data: {
-//                             title: e.message.title,
-//                             body: e.message.body,
-//                             data: e.message.board_id,
-//                             senderName: e.message.senderName,
-//                             tag: e.message.message_id,
-//                             icon_id: e.message.icon,
-//                             user_id: e.message.sender
-//                         },
-//                     });
-//                 } 
-//             }
-//         }
-//         emitter.emit('pusher-event',e)        
-//     });
-
-    
-//     const beamsClient = new PusherPushNotifications.Client({
-//       instanceId: "985c704d-7433-49c3-a7a7-8f40690e1b37",
-//     });
-    
-//     beamsClient.start()
-//         .then(() => beamsClient.addDeviceInterest('hello'))
-//         .then(() => console.log('Successfully registered and subscribed!'))
-//         .catch(console.error);
-    
-// }
 
 import Vue3TouchEvents from "vue3-touch-events";
-
-
-
-
 import mitt from 'mitt'
 window.emitter = mitt()
 import { required, max, min, confirmed } from '@vee-validate/rules';
@@ -146,7 +85,6 @@ app
 // Board //
 
 .component('Help', defineAsyncComponent(() => import('./components/Help/Help.vue')))
-
 // third party plugin
 .component("v-select", VueSelect)
 
@@ -154,24 +92,6 @@ app
 .use(i18n)
 .use(MasonryWall)
 .use(VueLazyload);
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
-//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
-// });
-
-/**
- * Finally, we will attach the application instance to a HTML element with
- * an "id" attribute of "app". This element is included with the "auth"
- * scaffolding. Otherwise, you will need to add an element yourself.
- */
 
 app.mount('#app');
 
