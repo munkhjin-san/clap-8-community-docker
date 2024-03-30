43867683c4ecdfa8e090b1f6d37cce8b42cd0ef5
tumur <tumur.ulambayar@glowd.co.jp>
2024-03-30 11:00:01 +0900

    auth fix

--- config/app.php
+++ config/app.php
@@ -220,5 +220,9 @@ return [
     |
     */
 
+    'aliases' => Facade::defaultAliases()->merge([
+        // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+    ])->toArray(),
 
 ];
