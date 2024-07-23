2a028ac4b4395ad047df5c99f39db605492f3d97
munkhjin-san <glowd.m.uranchimegu@gmail.com>
2024-07-23 09:20:16 +0900

    taskRecord pivot deleted at

--- app/Models/taskRecord.php
+++ app/Models/taskRecord.php
@@ -13,13 +13,23 @@ class taskRecord extends Model
 
     protected $guarded = [];
     public function executors(){
-        return $this->belongsToMany(User::class, 'task_users', 'record_id', 'user_id')->where('users.retire', 0)->wherePivot('supervisor', 0)->withPivot('id', 'comp_flag', 'late_answer', 'late_answer_custom', 'status_flag', 'comment')->select(['users.id as id', 'users.name','users.icon_id']);
+        return $this->belongsToMany(User::class, 'task_users', 'record_id', 'user_id')
+                    ->where('users.retire', 0)
+                    ->wherePivot('supervisor', 0)
+                    ->wherePivotNull('deleted_at')
+                    ->withPivot('id', 'comp_flag', 'late_answer', 'late_answer_custom', 'status_flag', 'comment')
+                    ->select(['users.id as id', 'users.name','users.icon_id']);
     }
     public function files(){
         return $this->belongsToMany(FileRecord::class, 'task_use_files', 'record_id', 'file_id');
     } 
     public function supervisors() {
-        return $this->belongsToMany(User::class, 'task_users', 'record_id', 'user_id')->where('users.retire', 0)->wherePivot('supervisor', 1)->withPivot('id', 'comp_flag', 'late_answer', 'late_answer_custom', 'status_flag', 'comment')->select(['users.id as id', 'users.name','users.icon_id']);
+        return $this->belongsToMany(User::class, 'task_users', 'record_id', 'user_id')
+                    ->where('users.retire', 0)
+                    ->wherePivot('supervisor', 1)
+                    ->wherePivotNull('deleted_at')
+                    ->withPivot('id', 'comp_flag', 'late_answer', 'late_answer_custom', 'status_flag', 'comment')
+                    ->select(['users.id as id', 'users.name','users.icon_id']);
     }
     public function repeat(){
         return $this->hasOne(TaskRepeat::class, 'record_id', 'repeat_id');
