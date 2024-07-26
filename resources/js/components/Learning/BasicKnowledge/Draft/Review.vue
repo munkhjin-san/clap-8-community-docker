5eb82f7d01e36cf34d7cca68d8337446d6adc34d
munkhjin-san <glowd.m.uranchimegu@gmail.com>
2024-07-26 13:02:09 +0900

    learning review

--- resources/js/components/Learning/BasicKnowledge/Draft/Review.vue
+++ resources/js/components/Learning/BasicKnowledge/Draft/Review.vue
@@ -45,7 +45,7 @@
                 v-if="selectedTopic && portfolio && selectedTopic.assistant_id" 
                 :assistand-id="selectedTopic.assistant_id" 
                 :soure-text="portfolio?.ai_review_pre" 
-                :message="portfolio?.content + portfolio?.episode"
+                :message="storyContent + episodeContent"
                 :confirm-text="'発表用ポートフォリオは、研修テーマに沿った内容であり、発表時間が５分程度の内容にまとめられている。'"
                 ref="reviewEl"
             />
