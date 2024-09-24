1ffaad12cba85307f7eb229fb095111b0cbb2162
munkhjin-san <glowd.m.uranchimegu@gmail.com>
2024-09-24 09:30:20 +0900

    project incomplete development account added

--- resources/js/components/Board/IncompleteWindow.vue
+++ resources/js/components/Board/IncompleteWindow.vue
@@ -290,7 +290,7 @@ import ProjectGoalMore from "../Project/ProjectGoalMore.vue";
         router.push({name: 'timesheet', query: {user_id: user.id}})
     }
     const getProjectNotApproved = async() => {
-        if(auth && auth.activeUser.id == 631) {
+        if(auth && (auth.activeUser.id == 631 || auth.activeUser.id == 608)) {
             try {
                 const data = await axios.get('/project_not_approved').then(res => res.data)
                 notapprovedProjects.value = data
