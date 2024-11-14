b5e9a1ac7c8f64ef97b7227549098755d4a096fc
munkhjin-san <glowd.m.uranchimegu@gmail.com>
2024-11-14 16:14:36 +0900

    margin added to taskbox

--- resources/js/components/Board/Tray/Task/TaskBox.vue
+++ resources/js/components/Board/Tray/Task/TaskBox.vue
@@ -18,25 +18,19 @@
                                     <path d="M36.486 0.324c-0.666-0.515-1.629-0.396-2.204 0.22l-3.039 3.271-3.060 3.328c-2.031 2.23-4.067 4.452-6.086 6.689-2.025 2.234-8.487 9.367-9.743 10.772-0.132 0.15-0.369 0.129-0.486-0.025-1.060-1.399-2.287-3.028-3.468-4.519-1.161-1.465-2.516-3.22-3.271-4.144-0.755-0.927-1.702-2.093-2.191-2.668-0.528-0.625-1.457-0.791-2.182-0.329-0.765 0.489-0.973 1.521-0.518 2.307 0.367 0.636 2.307 3.801 2.307 3.801 0.801 1.27 3.213 5.039 3.699 5.791 0.487 0.751 1.194 1.782 1.879 2.788 0.684 1.004 1.52 2.313 2.429 3.264s2.487 0.627 3.321-0.358c1.932-2.282 9.588-11.527 11.498-13.857 1.916-2.327 3.815-4.668 5.719-7.004l2.842-3.517 2.823-3.535c0.548-0.687 0.451-1.716-0.272-2.276z"></path>
                                 </svg>                                           
                             </div>
-                            <!-- <div title="タスク承認待ち" v-if="user.pivot.status_flag == 1"  class="completed-badge" style="background-color: tomato;">
-                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="5px" viewBox="0 0 38 32" style="fill:#fff;margin:auto;">
-                                    <path d="M36.486 0.324c-0.666-0.515-1.629-0.396-2.204 0.22l-3.039 3.271-3.060 3.328c-2.031 2.23-4.067 4.452-6.086 6.689-2.025 2.234-8.487 9.367-9.743 10.772-0.132 0.15-0.369 0.129-0.486-0.025-1.060-1.399-2.287-3.028-3.468-4.519-1.161-1.465-2.516-3.22-3.271-4.144-0.755-0.927-1.702-2.093-2.191-2.668-0.528-0.625-1.457-0.791-2.182-0.329-0.765 0.489-0.973 1.521-0.518 2.307 0.367 0.636 2.307 3.801 2.307 3.801 0.801 1.27 3.213 5.039 3.699 5.791 0.487 0.751 1.194 1.782 1.879 2.788 0.684 1.004 1.52 2.313 2.429 3.264s2.487 0.627 3.321-0.358c1.932-2.282 9.588-11.527 11.498-13.857 1.916-2.327 3.815-4.668 5.719-7.004l2.842-3.517 2.823-3.535c0.548-0.687 0.451-1.716-0.272-2.276z"></path>
-                                </svg>                                           
-                            </div>                                    -->
+                                                           -->
                         </div>                                                                                       
                         <p style="margin-top:2px;cursor:pointer;font-size: 12px;margin-left: 3px;" v-if="taskUsers && taskUsers.length > 3">({{taskUsers.length}})</p>                                            
                     </div> 
                 </div>
 
                 
-                <div :style="{height: `${dynamicHeight}`, overflow: 'hidden', transition: 'height 0.1s ease'}">
+                <div :style="{height: `${dynamicHeight}`, overflow: 'hidden', transition: 'height 0.1s ease', marginTop: '10px'}">
                     <p ref="taskBody" style="line-height: 1.5;white-space: pre-line;" v-html="truncatedRemarks"></p>
                 </div>  
                 <div @click="setTruncate" class="jump-link" style="margin-top:10px" v-if="dynamicHeight !== 'auto'">{{ dynamicHeight == '55px' ? '続きを表示する' : '閉じる' }}</div>
 
-                <!-- <div v-if="item.end_at && item.repeat" style="margin-top: 10px;">
-                    <div :style="{fontSize: '12px', color: dateColor}">現在のタスクは{{dateDetail(item.end_at)}}</div>
-                </div> -->
+              
                 <div v-if="item.end_at" style="margin-top: 10px;">
                     <div :style="{fontSize: '12px', color: dateColor}">{{dueDetail}}</div>                         
                 </div>
