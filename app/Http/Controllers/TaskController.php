81254b7cd8a3fcf4f1ecead0295d97873602fcff
munkhjin-san <glowd.m.uranchimegu@gmail.com>
2024-11-15 13:59:55 +0900

    fix task for users

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -39,20 +39,24 @@ class TaskController extends Controller
         $which = $request->which;
         if(!empty($request) && !empty($auth_user_id)){      
             $tasks = taskRecord::where('board_id', $request->record_id)
-            // ->whereNotNull(columns: 'end_at')
             ->where(function ($query) use ($auth_user_id, $which) {
-                $query->whereHas('executors', function($q) use ($auth_user_id, $which) {
-                    $q->where('comp_flag', $which)
-                        ->where('user_id', $auth_user_id);
-                })->orWhere(function ($query) use ($auth_user_id, $which) {
-                    $query->whereDoesntHave('executors', function($q) use ($auth_user_id) {
-                        $q->where('user_id', $auth_user_id);
-                    })->where('comp_flag', $which);
+                $query->where(function ($subQuery) use ($auth_user_id, $which) {
+                    $subQuery->whereNotNull('end_at')
+                        ->whereHas('executors', function ($q) use ($auth_user_id, $which) {
+                            $q->where('comp_flag', $which)
+                                ->where('user_id', $auth_user_id);
+                        });
+                })
+                ->orWhere(function ($subQuery) use ($auth_user_id, $which) {
+                    $subQuery->whereNull('end_at')
+                        ->orWhere(function ($query) use ($auth_user_id, $which) {
+                            $query->whereDoesntHave('executors', function ($q) use ($auth_user_id) {
+                                $q->where('user_id', $auth_user_id);
+                            })->where('comp_flag', $which);
+                        });
                 });
             })
-            ->with('executors')
-            ->with('files')
-            ->with('supervisors')
+            ->with(['executors', 'files', 'supervisors'])
             ->when($request->which == 1, function($q){
                 $q->onlyTrashed();                    
             })
