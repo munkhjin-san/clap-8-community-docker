6b5272346eee2f6062c726081deca3091c6c336b
munkhjin-san <glowd.m.uranchimegu@gmail.com>
2025-02-26 16:33:40 +0900

    if upgrade shokkai

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -282,7 +282,7 @@ class ProjectController extends Controller
                             $q->with(['evaluation' => function ($q) use($params) {
                                 $q->where('year', $params['year'])
                                     ->where('which_half', $params['which_half'])
-                                    ->with('mentor');
+                                    ->with('mentor', 'candidate');
                             }]);
                         })                        
                         ->with('positions')
