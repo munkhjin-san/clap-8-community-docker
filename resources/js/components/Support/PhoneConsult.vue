fa4f76e73d129afd93b36c8265bba05ea1ba4758
tumur <glowd.u.tumur@gmail.com>
2025-06-30 08:59:57 +0900

    Revert "support desk changes"
    This reverts commit f6c339cdf88f942ad23a15f27f496d9f9212c1a7.
    
    a

--- resources/js/components/Support/PhoneConsult.vue
+++ resources/js/components/Support/PhoneConsult.vue
@@ -2,64 +2,38 @@
     <div class="support-content">
         <div class="support-title">電話相談</div>
         <div class="support-content-inner">
-            <div class="mobile-tel-title">【経営管理本部へのご相談】</div>
+            <div class="mobile-tel-title">社内相談</div>
                 <div style="line-height:1.3;margin-top: 10px;">
                     <a href="tel:092-291-1355">092-291-1355</a>
                 </div>
 
-                <div class="mt-[20px] leading-[2]">
+                <div class="mobile-tel-title" style="margin-top:20px;">社外相談</div>
+                <div class="tell-content" style="line-height:2; margin-left:20px;">
 
-                    <p>【業務災害総合保険の付帯サービス】グラウドの従業員のみ利用可能です。</p>
+                    <p>匿名での相談を希望される方は、下記の社外相談窓口をご利用ください。</p>
 
-                    <p><span><a href="tel:0120-437-213">0120-437-213</a></span></p>
+                    <p>社外相談窓口：社会保険労務士法人B-GROOW　代表　森　恵子（もり　けいこ）様</p>
 
-                    <p class="whitespace-break-spaces">※詳細は添付ファイルをご確認ください。<br>
-                        会員番号をヒアリングされた場合は「24Q2E8A」とお伝えください。<br><br>
+                    <p>連絡先：<span><a href="tel:090-4343-2439">090-4343-2439</a></span></p>
 
-                        ①24時間電話健康相談　介護相談ホットライン<br>
-                        ②女性のための24時間電話健康相談<br>
-                        ③セカンドオピニオンアレンジサービス<br>
-                        ④メンタルケアカウンセリングサービス<br>
-                        ⑤生活習慣病サポートサービス<br>
-                        ⑥がん治療と仕事の両立支援サービス<br>
-                        ⑦二次検針機関の手配サービス<br>
-                    </p>
+                    <p>受付時間帯：9時〜18時（平日のみ）</p>
+
+                    <p>【社外相談窓口機能】</p>
+
+                    <p>一般事例及び労働基準法をもとに、相談者からのヒアリングおよび労務的アドバイスを行います。</p>
+
+                    <p>相談内容を纏め、相談者の希望する範囲内でグラウドへ共有、対応依頼を行います。</p>
+
+                    <p>相談者の希望範囲において、相談後の対応状況や結果の共有を行います。</p>
 
                 </div>
                 
-                <div class="mobile-tel-title" style="line-height:2.8;margin-top:20px;">【完全匿名のご相談】</div>
+                <div class="mobile-tel-title" style="line-height:2.8;margin-top:20px;">全国労働基準監督署</div>
 
                 <div style="line-height:2">
-                    <a href="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/koyou_roudou/roudoukijun/location.html" target="_blank">全国労働基準監督署</a>
-                </div>
-                <div class="mt-[20px]">
-                    <Files :items="files" path="support_files" />
+                    <a href="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/koyou_roudou/roudoukijun/location.html" target="_blank">全国労働基準監督署公式サイト</a>
                 </div>
 
         </div>
     </div>
 </template>
-<script setup lang="ts">
-import Files from '../Global/Files.vue';
-const files = [
-    {
-        id: 1,
-        name: '付帯サービスご案内チラシ.pdf',
-        path: 'service_guide',
-        extension: 'pdf',
-        size: 1561361,
-        mime_type: 'application',
-        user_id: 608,
-    },
-    {
-        id: 2,
-        name: '従業員様向けカード.pdf',
-        path: 'employee_card',
-        extension: 'pdf',
-        size: 772668,
-        mime_type: 'application',
-        user_id: 608,
-    }
-
-]
-</script>
