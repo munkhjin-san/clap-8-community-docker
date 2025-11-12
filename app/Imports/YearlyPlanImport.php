7f0d3252057d8ab5ab2d45d01624d880bb296f5a
tumur1 <tumur.ulambayar@glowd.co.jp>
2025-11-12 13:09:12 +0900

    feat(Imports): implement WithCalculatedFormulas interface in YearlyPlanImport class

--- app/Imports/YearlyPlanImport.php
+++ app/Imports/YearlyPlanImport.php
@@ -4,8 +4,9 @@ namespace App\Imports;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
 
-class YearlyPlanImport implements ToCollection
+class YearlyPlanImport implements ToCollection, WithCalculatedFormulas
 {
     /**
     * @param Collection $collection
