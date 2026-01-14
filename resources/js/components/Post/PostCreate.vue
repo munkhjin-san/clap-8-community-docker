056a620034a499e2f349548583b488d9d1df5be5
tumur1 <tumur.ulambayar@glowd.co.jp>
2026-01-14 12:14:33 +0900

    feat: Add confirmation prompt before post creation and refine post menu actions based on app type

--- resources/js/components/Post/PostCreate.vue
+++ resources/js/components/Post/PostCreate.vue
@@ -473,6 +473,8 @@ import { User } from '@/interface/globalInterface'
             ping('入力内容に不備があります。')
             return
         }
+        const confirm = await ask('入力内容には間違いないかを確認してください。\n作成後は編集できません。')
+        if (!confirm.value) return
         processing.value = true      
 
             
