369c49c6b2d803ecc0aa3b4b97761828e2230337
munkhjin-san <glowd.m.uranchimegu@gmail.com>
2026-04-01 08:18:19 +0900

    fix: Import DriveDownloadLog model in PruneActivityLogs command

--- app/Console/Commands/PruneActivityLogs.php
+++ app/Console/Commands/PruneActivityLogs.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\DriveActivityLog;
+use App\Models\DriveDownloadLog; 
 class PruneActivityLogs extends Command
 {
     /**
