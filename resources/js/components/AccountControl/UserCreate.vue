7e39d722d4ac7231fc7d7cc1724edcd2a86558f9
munkhjin-san <glowd.m.uranchimegu@gmail.com>
2026-04-01 08:40:04 +0900

    fix: Show MemberSelector only for non-partner users

--- resources/js/components/AccountControl/UserCreate.vue
+++ resources/js/components/AccountControl/UserCreate.vue
@@ -184,8 +184,9 @@
                     />
                 </div>
 
-                <div v-if="!isPartner" style="padding: 15px;border: solid thin tomato;" class="si-box">
+                <div style="padding: 15px;border: solid thin tomato;" class="si-box">
                     <MemberSelector 
+                        v-if="!isPartner"
                         placeHolder="サブアカウント"
                         v-model="subParams.linked"
                         :options="linkables"
