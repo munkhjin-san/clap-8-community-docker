6fa65677479ed6ee4f04f1bf36ef918e4f4f5cb3
tumur <glowd.u.tumur@gmail.com>
2026-08-04 11:29:17 +0900

    feat(Rakuaward): add announcement endpoint for Rakuaward results
    - Introduced a new POST endpoint `/rakuaward_announce` in the Rakuaward routes to facilitate the announcement of results by directors.
    - This addition enhances the functionality for managing Rakuaward results and improves user interaction with the announcement process.

--- routes/web.php
+++ routes/web.php
@@ -404,6 +404,7 @@ Route::group(["middleware"=> ["auth", "session.expired"]],function(){
         Route::post('/rakuaward_score', [PostController::class, 'rakuaward_score']);
         Route::get('/rakuaward_mvps', [PostController::class, 'rakuaward_mvps']);
         Route::post('/rakuaward_result_read', [PostController::class, 'rakuaward_result_read']);
+        Route::post('/rakuaward_announce', [PostController::class, 'rakuaward_announce']);
         Route::post('/post_get_post_users', [PostController::class, 'post_get_post_users']);
         Route::post('/post_get_all_possible_users', [PostController::class, 'post_get_all_possible_users']);
         Route::post('/post_get_challenge_users', [PostController::class, 'post_get_challenge_users']);
