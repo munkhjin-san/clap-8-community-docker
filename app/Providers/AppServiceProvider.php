4e39d98a476e1b6c2320bdc695b122a65e23d6ad
tumur <glowd.u.tumur@gmail.com>
2026-08-17 13:54:07 +0900

    refactor(AppServiceProvider): rename ActualResultCsvService to ActualResultCalculationService
    Updated the service import in AppServiceProvider to reflect the renaming of ActualResultCsvService to ActualResultCalculationService, improving clarity and consistency in service naming.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Notifications\Messages\MailMessage;
 use App\Services\ActualReserveAllocationService;
-use App\Services\ActualResultCsvService;
+use App\Services\ActualResultCalculationService;
 use App\Services\SharedService;
 use App\Services\BoardControllerProxy;
 use GuzzleHttp\Client as Http;
